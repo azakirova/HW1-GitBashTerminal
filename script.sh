444384578b9e56b7485c8dd544dafab136d111a5
Anasteisha <akov93@gmail.com>
2022-05-31 15:59:17 +0700

    Initial commit Add files HW1, HW2, script

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+mkdir scripts
+cd scripts
+mkdir test1 test2 test3
+cd test1
+touch file1.txt file2.txt file3.txt file4.json file5.json
+mkdir folder1 folder2 folder3
+ls -la
+mv file1.txt file2.txt folder1
+
